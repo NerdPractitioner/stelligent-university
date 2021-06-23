3ed9bab2bb62278c93cc9324ada2934a38eeea04
EC2 Default User <ec2-user@ip-10-0-0-156.ec2.internal>
2021-06-23 17:33:50 +0000

    Remove env files

--- 00-dev-environment/helloworld/myenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/ec2-user/environment/stelligent-university/00-dev-environment/helloworld/myenv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
