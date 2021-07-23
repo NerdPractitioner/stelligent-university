aa872ab5c165e8b41602a7ead53267377dcccb20
EC2 Default User <ec2-user@ip-10-0-0-75.ec2.internal>
2021-07-23 19:04:05 +0000

    Refactored lab 9-2-2 code to fix s3 permission errors

--- 09-lambda/923/index.py
+++ 09-lambda/923/index.py
@@ -1,19 +1,21 @@
 import json
 import boto3
-
-dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('s3log')
+from boto3.dynamodb.conditions import Key
 
 def lambda_handler(event, context):
-    
-    response = table.scan()
-    data = response['Items']
 
-    while 'LastEvaluatedKey' in response:
-       response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
-       data.extend(response['Items'])
-    
-    return {
-        "statusCode": 200,
-        "body": json.dumps(data)
-    }
+    data = json.loads(event["body"])
+
+    dynamodb = boto3.resource('dynamodb')
+
+    table = dynamodb.Table('holmes-demo-data')
+
+    print(f"Getting item with filter: {data}")
+
+    response = table.query(
+        KeyConditionExpression=Key('event_source').eq(data["event_source"]))["Items"]
+
+    response = {'statusCode': 200,
+    'body': json.dumps(response)}
+
+    return response
